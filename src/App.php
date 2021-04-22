088fa97348f548daad95537b49c48b2d56460537
tivins <tivins@elabo.fr>
2021-04-22 14:23:24 +0200

    adding App.php

---
+++ src/App.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tivins\Framework;
+
+use Tivins\Database;
+use Parsedown;
+
+class App
+{
+    private static Database  $db;
+    private static Router    $router;
+    private static Logger    $logger;
+    private static Parsedown $parsedown;
+
+    public static function init()
+    {
+        sessionInit();
+        self::$router = new Router();
+    }
+
+    public static function initDB(Connector $connector) { self::$db = new Database($connector); }
+    public static function initMarkdown() { self::$parsedown = new Parsedown(); }
+
+    public static function markdown($str) : string
+    {
+        Hooks::run('pre-markdown', $str);
+        $str = self::$parsedown->text($str);
+        Hooks::run('post-markdown', $str);
+        return $str;
+    }
+
+    public static function router() : Router { return self::$router; }
+    public static function db() : Database { return self::$db; }
+    public static function logger() : Logger { return self::$logger; }
+    public static function setLogger(Logger $logger) : void { self::$logger = $logger; }
+}
