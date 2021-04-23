a65cbf87d7ca631b557b299a43a72ed6354f89e5
tivins <tivins@elabo.fr>
2021-04-23 17:07:07 +0200

    Adding src/Enum.php

---
+++ src/Enum.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tivins\Framework;
+
+class Enum
+{
+    private static $_names;
+
+    protected static function remap()
+    {
+        $oClass = new \ReflectionClass(get_called_class());
+        $consts = $oClass->getConstants();
+        self::$_names = array_flip($consts);
+        var_dump(self::$_names);
+    }
+
+    public static function toString(int $state) : string
+    {
+        if (!isset(self::$_names)) self::remap();
+        return self::$_names[$state] ?? (string) $state;
+    }
+}
